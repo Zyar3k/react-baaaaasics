449cb7d85b2523e777076bb2218ca27758c5d404
zyar3k <a.zygan@gmail.com>
2022-01-25 12:05:00 +0100

    React basics

---
+++ index.js
@@ -0,0 +1,15 @@
+const page = (
+  <div>
+    <img src="./logo192.png" width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Ha well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
